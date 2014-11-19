facd0cb51969d2335ce884fc5ca6e042d01ddcbe
Julien Blanchard <julien@sideburns.eu>
2014-11-19 11:54:18 +0100

    Add lein-typed plugin

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,5 @@
-{:user {:plugins [[lein-exec "0.3.2"] [lein-try "0.4.1"] [lein-bin "0.3.4"] [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
+{:user {:plugins [[lein-exec "0.3.2"]
+                  [lein-try "0.4.1"]
+                  [lein-bin "0.3.4"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [lein-typed "0.3.5"]]}}
