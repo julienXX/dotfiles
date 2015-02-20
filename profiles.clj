f4d6336ac760a44e3df5c6a2175853ae50a67a8f
Julien Blanchard <julien@sideburns.eu>
2015-02-20 23:57:15 +0100

    Update leiningen global plugins

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[lein-exec "0.3.2"]
-                  [lein-try "0.4.1"]
+{:user {:plugins [[lein-exec "0.3.4"]
+                  [lein-try "0.4.3"]
                   [lein-bin "0.3.4"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-typed "0.3.5"]]}}
+                  [cider/cider-nrepl "0.8.2"]
+                  [lein-typed "0.3.5"]
+                  [codox "0.8.10"]]}
+ :signing {:gpg-key "6D45BFDD"}}
