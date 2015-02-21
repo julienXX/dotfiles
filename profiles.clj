fec9e3545a59aec076dc99819bf0c399c6d737f9
Julien Blanchard <julien@sideburns.eu>
2015-02-21 15:47:39 +0100

    Add lein-cljfmt

--- profiles.clj
+++ profiles.clj
@@ -3,5 +3,6 @@
                   [lein-bin "0.3.4"]
                   [cider/cider-nrepl "0.8.2"]
                   [lein-typed "0.3.5"]
-                  [codox "0.8.10"]]}
+                  [codox "0.8.10"]
+                  [lein-cljfmt "0.1.10"]]}
  :signing {:gpg-key "6D45BFDD"}}
