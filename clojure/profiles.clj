35300ef56e21b457bd30c43b7a3b9ca91815ad77
Julien Blanchard <julien@sideburns.eu>
2019-11-04 14:42:24 +0100

    Clojure | Update repl version

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-exec "0.3.4"]
                   [lein-try "0.4.3"]
                   [lein-bin "0.3.4"]
-                  [cider/cider-nrepl "0.8.2"]
+                  [nrepl "0.6.0"]
+                  [cider/cider-nrepl "0.21.1"]
                   [lein-typed "0.3.5"]
                   [codox "0.8.10"]
                   [lein-cljfmt "0.1.10"]]}
