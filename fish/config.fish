34a8e50a7bb3b3d5ce51ec2f498da1cebaa52705
Julien Blanchard <julien@typed-hole.org>
2021-05-29 17:19:34 +0200

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,46 @@
+set -x MOZ_ENABLE_WAYLAND 1
+set -x XDG_SESSION_TYPE wayland
+set -x QT_QPA_PLATFORM wayland-egl
+set -x ELM_DISPLAY wl
+set -x LIBSEAT_BACKEND logind
+
+fish_add_path /home/julien/.cask/bin
+
+function mcd --wraps mkdir --description 'alias mcd=mkdir && cd'
+    mkdir $argv && cd $argv
+end
+
+function findrm
+    find . -iname "$argv" -exec rm -r {} \;
+end
+
+function pkg
+    switch $argv[1]
+        case search
+	          xbps-query -Rs $argv[2]
+	      case sync
+            doas xbps-install -Su
+	      case add
+	          doas xbps-install -Su $argv[2]
+	      case remove
+	          doas xbps-remove -R $argv[2]
+	      case autoremove
+	          doas xbps-remove -Oo
+	      case '*'
+            printf 'Unknown command: %s\n' $argv[1]
+    end
+end
+
+alias psgrep="ps aux | grep"
+alias anonradio="mpv http://anonradio.net:8000/anonradio"
+alias tilderadio="mpv https://radio.tildeverse.org/radio/8000/radio.ogg"
+alias droogies="mpv http://www.droogies.org:8000/stream"
+alias tv="mpv http://baud.vision:21225 --really-quiet"
+
+fish_ssh_agent
+source $HOME/.config/fish/autojump.fish
+
+if test (tty) = "/dev/tty1"
+    brightnessctl set 40%
+    hikari
+end
