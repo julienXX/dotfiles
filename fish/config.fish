f66ba0ec7a594515eec12ebdbf7921272f628312
Julien Blanchard <julien@typed-hole.org>
2021-09-19 17:21:34 +0200

    Fish | Add fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,13 @@
 set -x MOZ_ENABLE_WAYLAND 1
-set -x XDG_RUNTIME_DIR /tmp
+set -x MOZ_WAYLAND_USE_VAAPI 1
+set -x XDG_RUNTIME_DIR /tmp/1000
 set -x XDG_SESSION_TYPE wayland
 set -x QT_QPA_PLATFORM wayland-egl
 set -x ELM_DISPLAY wl
 set -x LIBSEAT_BACKEND seatd
+set -x LIBVA_DRIVER_NAME radeonsi
 
-fish_add_path /home/julien/.cask/bin
+fish_add_path /home/julien/.cargo/bin
 
 function mcd --wraps mkdir --description 'alias mcd=mkdir && cd'
     mkdir $argv && cd $argv
@@ -20,11 +22,15 @@ alias anonradio="mpv http://anonradio.net:8000/anonradio"
 alias tilderadio="mpv https://radio.tildeverse.org/radio/8000/radio.ogg"
 alias droogies="mpv http://www.droogies.org:8000/stream"
 alias tv="mpv http://baud.vision:21225 --really-quiet"
+alias pkg="doas vpm"
 
 fish_ssh_agent
 source $HOME/.config/fish/autojump.fish
+source $HOME/.config/fish/functions/prompt.fish
 
 if test (tty) = "/dev/tty1"
-    brightnessctl set 40%
+    brightnessctl set 30%
+    mkdir -p /tmp/1000
     hikari
+    #startx
 end
---
+++ fish/functions/prompt.fish
@@ -0,0 +1,30 @@
+# Options
+set __fish_git_prompt_showcolorhints
+
+# Colors
+set green (set_color green)
+set magenta (set_color magenta)
+set normal (set_color normal)
+set red (set_color red)
+set yellow (set_color yellow)
+
+set __fish_git_prompt_color_branch magenta --bold
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+  set_color -o purple
+  echo -n "λ "
+  set_color normal
+end
